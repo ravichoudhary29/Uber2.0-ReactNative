798c238a2828c2b719010c35b4f93f4ad17de276
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-05-06 22:53:12 +0530

    Build the Home screen component

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen.js";
 import { store } from "./store";
 
 // Setup Redux
 export default function App() {
     return (
         <Provider store={store}>
-            <View style={styles.container}>
-                <Text>Lets build </Text>
-            </View>
+            <HomeScreen />
         </Provider>
     );
 }
