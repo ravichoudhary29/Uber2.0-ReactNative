c7f36aea8ff0e8896fa54349befa8f3ad72752ea
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-05-07 19:15:41 +0530

    Implement React Native Navigation

--- App.js
+++ App.js
@@ -4,14 +4,32 @@ import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen.js";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MapScreen from "./screens/MapScreen.js";
 
 // Setup Redux
 export default function App() {
+    const Stack = createStackNavigator();
     return (
         <Provider store={store}>
-            <SafeAreaProvider>
-                <HomeScreen />
-            </SafeAreaProvider>
+            <NavigationContainer>
+                <SafeAreaProvider>
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name="HomeScreen"
+                            component={HomeScreen}
+                            options={{ headerShown: false }}
+                        />
+                        <Stack.Screen
+                            name="MapScreen"
+                            component={MapScreen}
+                            options={{ headerShown: false }}
+                        />
+                    </Stack.Navigator>
+                </SafeAreaProvider>
+            </NavigationContainer>
         </Provider>
     );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,13 +2,14 @@ import { Text, View, FlatList, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
     {
         id: "123",
         title: "Get a ride",
         image: "https://links.papareact.com/3pn",
-        screen: "Mapscreen",
+        screen: "MapScreen",
     },
     {
         id: "456",
@@ -19,6 +20,7 @@ const data = [
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
     return (
         <FlatList
             data={data}
@@ -26,6 +28,7 @@ const NavOptions = () => {
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
                 <TouchableOpacity
+                    onPress={() => navigation.navigate(item.screen)}
                     style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 h-64`}>
                     <View>
                         <Image
