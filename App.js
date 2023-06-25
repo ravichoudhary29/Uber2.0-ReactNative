db74f519112f6930e83a4c059333feaf051c5d02
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-06-25 12:55:34 +0530

    need to fetch api's of google services for the app

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Provider } from "react-redux";
-import HomeScreen from "./screens/HomeScreen.js";
-import { store } from "./store";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import MapScreen from "./screens/MapScreen.js";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+import HomeScreen from './screens/HomeScreen.js'
+import { store } from './store'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import MapScreen from './screens/MapScreen.js'
 
 // Setup Redux
 export default function App() {
-    const Stack = createStackNavigator();
+    const Stack = createStackNavigator()
     return (
         <Provider store={store}>
             <NavigationContainer>
@@ -31,14 +31,14 @@ export default function App() {
                 </SafeAreaProvider>
             </NavigationContainer>
         </Provider>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-});
+})
